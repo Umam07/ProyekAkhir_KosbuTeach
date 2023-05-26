c2a8c83130072f713270479f94049c66aaf43398
RAFLY <RAFLY@DESKTOP-0D0DNCR>
2023-05-26 22:48:09 +0700

    Basis Data

--- src/sql/street_attire_co.sql
+++ src/sql/street_attire_co.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Bulan Mei 2023 pada 06.40
--- Versi server: 10.4.28-MariaDB
--- Versi PHP: 8.2.4
+-- Generation Time: May 26, 2023 at 05:47 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,20 +24,85 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data`
+-- Table structure for table `data`
 --
 
 CREATE TABLE `data` (
+  `id` int(50) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(50) NOT NULL,
+  `nomor_telepon` varchar(12) NOT NULL,
+  `alamat` longtext NOT NULL,
+  `voucher` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `data`
+-- Dumping data for table `data`
 --
 
-INSERT INTO `data` (`username`, `password`) VALUES
-('coba', '123456789');
+INSERT INTO `data` (`id`, `username`, `password`, `nomor_telepon`, `alamat`, `voucher`) VALUES
+(1, 'Stellaron', '123456789', '008976354594', 'Dawuan Tengah', 'DJECO'),
+(2, 'Rafly', '123456789', '08976354594', 'Cikampek', 'DJECO'),
+(3, 'udin', '11111111111', '0', '', 'DJECO'),
+(4, 'ikan', '111111111', ' ', '', 'DJECO'),
+(5, 'Tomat', '111111111', '08976354594', 'Dawuan Tengah Cikampke Karawng ', 'DJECO');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `id` int(50) NOT NULL,
+  `produk` varchar(50) NOT NULL,
+  `harga` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id`, `produk`, `harga`) VALUES
+(1, 'Action Figure Sukuna', 450000),
+(2, 'Sepatu Nike Original', 2500000),
+(3, 'Baju Anime Anya', 250000),
+(4, 'Sepatu Speedy 15', 1500000),
+(5, 'Baju AcmeDe Lavie', 1500000),
+(6, 'Tas Selempang Coach', 2000000),
+(7, 'Jam Tangan Rolex', 5000000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
